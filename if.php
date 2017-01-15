ba8f88c7a33df19012ea9fc67ae758400f024271
Jeanrhe <jeanrhe@gmail.com>
2017-01-15 09:34:07 -0500

    if
    Nos sirve para validar una condicion.

---
+++ if.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+  </head>
+  <body>
+    <p>
+      <?php
+        $items = 6;   // Si la condición de la línea 9 es verdadera, se ejecutará el código que está entre las llaves.
+        if($items > 5) {
+          echo "10% de descuento";
+        }
+      ?>
+    </p>
+  </body>
+</html>

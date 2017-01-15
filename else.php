1fab6bcc0c7aeda2809135cb6c3f86a344389976
Jeanrhe <jeanrhe@gmail.com>
2017-01-15 09:36:25 -0500

    Else
    Si la validacion en if es falsa entonces pasa inmediatamente a la parte
    de else.

---
+++ else.php
@@ -0,0 +1,22 @@
+<html>
+  <head>
+  </head>
+  <body>
+    <p>
+      <?php
+        $items = 3;
+        
+        if($items > 5) 
+                {
+                echo "¡Conseguís 10% de descuento!";
+                }
+                  else    
+                    {
+                       echo "¡Conseguís un 5% de descuento!";
+                    }
+        
+        
+      ?>
+    </p>
+  </body>
+</html>

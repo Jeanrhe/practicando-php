f7c394f61e011931a13a2fb79530b197cbe20088
Jeanrhe <jeanrhe@gmail.com>
2017-01-16 12:18:39 -0500

    while-EndWhile
    Metodo para cerrar un ciclo while

---
+++ while_endwhile.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<title>Un ciclo tuyo</title>
+        <link type='text/css' rel='stylesheet' href='style.css'/>
+	</head>
+	<body>
+    <?php
+	//Agregá un ciclo while a continuación
+	
+	$conteoCiclo = 0;
+while ($conteoCiclo < 5):
+    $conteoCiclo ++;
+   echo $conteoCiclo ;
+    endwhile;
+  
+    
+    ?>
+    </body>
+</html>

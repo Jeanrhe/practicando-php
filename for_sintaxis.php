b9edae40485255e6d5bb45f14f64d984cb97f5fb
Jeanrhe <jeanrhe@gmail.com>
2017-01-16 12:00:50 -0500

    For_sintaxis
    Estructura de for.

---
+++ for_sintaxis.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title>Ciclos For</title>
+  </head>
+  <body>
+    <p>
+      <?php
+        // Muestra usando echo los primeros cinco números pares
+        for ($i = 2; $i < 11; $i = $i + 2){
+          echo $i;
+        }
+      ?>
+    </p>
+  </body>
+</html>

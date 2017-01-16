d63ade8418627c46d82090643a6634db916a2146
Jeanrhe <jeanrhe@gmail.com>
2017-01-16 11:58:22 -0500

    For
    Se utiliza para poder definir ciclos

---
+++ for.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>Años bisiestos</title>
+  </head>
+  <body>
+    <?php
+      for ($bisiesto = 2004; $bisiesto < 2050; $bisiesto = $bisiesto + 4) {
+        echo "<p>$bisiesto</p>";
+
+      }
+    ?>
+  </body>
+</html>

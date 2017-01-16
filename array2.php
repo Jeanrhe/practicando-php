c626962df43c83b9264c8557b1eb77411ae6ef12
Jeanrhe <jeanrhe@gmail.com>
2017-01-16 10:41:13 -0500

    array2
    Accedemos a un array $nombrearray[ ] ,

---
+++ array2.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>Acceder a elementos</title>
+  </head>
+  <body>
+    <p>
+      <?php
+        $decenas = array(10, 20, 30, 40, 50);
+        echo $decenas[2];
+      ?>
+    </p>
+  </body>
+</html>

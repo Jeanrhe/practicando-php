c76bc09cf48cfbea7fb4eeedb6d932fa9a5d1535
Jeanrhe <jeanrhe@gmail.com>
2017-01-16 10:22:25 -0500

    Switch comparando con variable
    Se hace una comparacion con una variable.

---
+++ switch2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+    <?php
+    $fruta = "Manzana";
+    
+    switch ($fruta) {
+        case 'Manzana':
+            echo "Rica.";
+            break;
+    default:
+            echo "No selecciono una fruta";  
+    }
+    
+    ?>
+    </body>
+</html>

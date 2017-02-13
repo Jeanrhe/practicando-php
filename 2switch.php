ef680800af5b592ae09b49a935a505fd62e9533e
Jeanrhe <jeanrhe@gmail.com>
2017-02-13 13:48:36 -0500

    ss
    sss

---
+++ 2switch.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+    <?php
+    switch (2) {
+        case 0:
+            echo 'El valor es 0';
+            break;
+        case 1:
+            echo 'El valor es 1';
+            break;
+        case 2:
+            echo 'El valor es 2';
+            break;
+        default:
+            echo "El valor no es ni 0, 1 ni 2";
+    }
+    ?>
+    </body>
+</html>
---
+++ 2switch4.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+    <?php
+    $i = 5;
+    
+    switch ($i):
+        case 0:
+            echo '$i es 0.';
+            break;
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+            echo '$i está en algún lado entre  1 y 5.';
+            break;
+        case 6:
+        case 7:
+            echo '$i es 6 o 7.';
+            break;
+        default:
+            echo '$i es igual o mayor que 8';
+    endswitch;
+    ?>
+    </body>
+</html>
